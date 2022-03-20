203f9db3029c14ca6992b99afb91c03a665f50ef
Can Iscan <caniscan_@hotmail.com>
2022-03-20 23:50:01 +0100

    Task 0

---
+++ PT/PT/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PT
+{
+	public class Calculator
+	{
+		public int Add(int x, int y)
+		{
+			return x + y;
+		}
+
+		public int Subtract(int x, int y)
+		{
+			return x - y;
+		}
+		
+		public int Multiply(int x, int y)
+		{
+			return x * y;
+		}
+
+		public double Divide(int x, int y)
+		{
+			if (y == 0)
+			{
+				throw new DivideByZeroException("Cannot divide by zero.");
+			}
+
+			return (double) x / y;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Programming Technology Lab
+
+## Team
+
+| Name Surname (initials) | GUID                                     |
+| ----------------------- | ---------------------------------------- |
+| CI                      | `{3C195BF7-3B0B-3943-C41D-E97B87BFC125}` |
